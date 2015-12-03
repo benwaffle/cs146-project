e15d121d65255654d8fab27b798e9987742558de
Ben Iofel <iofelben@gmail.com>
2015-12-02 20:35:40 -0500

    fix merge conflicts

--- script.js
+++ script.js
@@ -33,40 +33,22 @@ function summarize() {
                  id("state").value.trim() + " " +
                  id("zipcode").value.trim() + "<br />";
   str += "Meal Type: ";
-<<<<<<< HEAD
-  radios = document.getElementsByName("meal_type");
-  for (i = 0; i < radios.length; i = i + 1) {
-=======
   var radios = document.getElementsByName("meal_type");
   for (var i = 0; i < radios.length; i++) {
->>>>>>> origin/master
     if (radios[i].checked) {
       str += radios[i].value + "<br />";
       break;
     }
   }
   str += "Meal: ";
-<<<<<<< HEAD
-  checkboxes = document.getElementsByName("meal[]");
-  selected_meals = [];
-  for (i = 0; i < checkboxes.length; i = i + 1) {
-=======
   var checkboxes = document.getElementsByName("meal[]");
   var selected_meals = [];
   for (var i = 0; i < checkboxes.length; i++) {
->>>>>>> origin/master
     if (checkboxes[i].checked) {
       selected_meals.push(checkboxes[i].value);
     }
   }
   str += selected_meals + "<br />";
-<<<<<<< HEAD
-  str += "Date needed: " + document.getElementById("date_needed").value + "<br />";
-  str += "Quantity: " + document.getElementById("quantity").value + "<br />";
-  str += "Special Instructions: " +       document.getElementById("special_instructions").value.trim();
-  document.getElementById("summary").innerHTML = str;
-  document.getElementById("summary_wrapper").style.display = "block";
-=======
   str += "Date needed: " + id("date_needed").value + "<br />";
   str += "Quantity: " + id("quantity").value + "<br />";
   str += "Special Instructions: " + id("special_instructions").value.trim();
@@ -77,7 +59,6 @@ function summarize() {
   var random = Math.floor(Math.random() * results.length);
   id("hand_name").innerHTML = results[random].name;
   id("hand_image").src = 'images/Hands/' + results[random].file;
->>>>>>> origin/master
 }
 
 function reset_form() {
