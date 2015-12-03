f6ade49099b82399b7f708fe3f9df439016cf633
susana- <susrest3@gmail.com>
2015-12-02 20:10:11 -0500

    Got the javascript to work!

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function summarize() {
                  document.getElementById("zipcode").value.trim() + "<br />";
   str += "Meal Type: ";
   radios = document.getElementsByName("meal_type");
-  for (i = 0; i < radios.length; i + 1) {
+  for (i = 0; i < radios.length; i = i + 1) {
     if (radios[i].checked) {
       str += radios[i].value + "<br />";
       break;
@@ -22,7 +22,7 @@ function summarize() {
   str += "Meal: ";
   checkboxes = document.getElementsByName("meal[]");
   selected_meals = [];
-  for (i = 0; i < checkboxes.length; i + 1) {
+  for (i = 0; i < checkboxes.length; i = i + 1) {
     if (checkboxes[i].checked) {
       selected_meals.push(checkboxes[i].value);
     }
@@ -31,8 +31,6 @@ function summarize() {
   str += "Date needed: " + document.getElementById("date_needed").value + "<br />";
   str += "Quantity: " + document.getElementById("quantity").value + "<br />";
   str += "Special Instructions: " +       document.getElementById("special_instructions").value.trim();
-  console.log(str);
-  console.log("test");
   document.getElementById("summary").innerHTML = str;
   document.getElementById("summary_wrapper").style.display = "block";
 }
