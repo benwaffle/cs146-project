1c56b73981c9e8aa4e7cebfd3c79f59294c55d1e
Ben Iofel <iofelben@gmail.com>
2015-12-02 20:16:54 -0500

    Fix and clean up form javascript

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
+function id(id) {
+  return document.getElementById(id);
+}
+
 function summarize() {
-  "use strict";
-  var i, str, field, radios, checkboxes, selected_meals;
-  str = "Summary: " + document.getElementById("firstname").value.trim() + " " +
-                    document.getElementById("lastname").value.trim() + "<br />" +
-                    document.getElementById("address1").value.trim() + "<br />";
-  field = document.getElementById("address2").value;
+  var str = "Summary: " + id("firstname").value.trim() + " " +
+                    id("lastname").value.trim() + "<br />" +
+                    id("address1").value.trim() + "<br />";
+  var field = id("address2").value;
   if (field !== null && field.trim() !== "") {
     str += field + "<br />";
   }
-  str += document.getElementById("city").value.trim() + ", " +
-                 document.getElementById("state").value.trim() + " " +
-                 document.getElementById("zipcode").value.trim() + "<br />";
+  str += id("city").value.trim() + ", " +
+                 id("state").value.trim() + " " +
+                 id("zipcode").value.trim() + "<br />";
   str += "Meal Type: ";
-  radios = document.getElementsByName("meal_type");
-  for (i = 0; i < radios.length; i + 1) {
+  var radios = document.getElementsByName("meal_type");
+  for (var i = 0; i < radios.length; i++) {
     if (radios[i].checked) {
       str += radios[i].value + "<br />";
       break;
     }
   }
   str += "Meal: ";
-  checkboxes = document.getElementsByName("meal[]");
-  selected_meals = [];
-  for (i = 0; i < checkboxes.length; i + 1) {
+  var checkboxes = document.getElementsByName("meal[]");
+  var selected_meals = [];
+  for (var i = 0; i < checkboxes.length; i++) {
     if (checkboxes[i].checked) {
       selected_meals.push(checkboxes[i].value);
     }
   }
   str += selected_meals + "<br />";
-  str += "Date needed: " + document.getElementById("date_needed").value + "<br />";
-  str += "Quantity: " + document.getElementById("quantity").value + "<br />";
-  str += "Special Instructions: " +       document.getElementById("special_instructions").value.trim();
-  console.log(str);
-  console.log("test");
-  document.getElementById("summary").innerHTML = str;
-  document.getElementById("summary_wrapper").style.display = "block";
+  str += "Date needed: " + id("date_needed").value + "<br />";
+  str += "Quantity: " + id("quantity").value + "<br />";
+  str += "Special Instructions: " + id("special_instructions").value.trim();
+
+  id("summary").innerHTML = str;
+  id("summary_wrapper").style.display = "block";
 }
 
 function reset_form() {
-  "use strict";
-  document.getElementById("summary").innerHTML = "";
-  document.getElementById("summary_wrapper").style.display = "none";
+  id("summary").innerHTML = "";
+  id("summary_wrapper").style.display = "none";
 }
